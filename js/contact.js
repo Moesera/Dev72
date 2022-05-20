3335f1dd64b89d91731610b1a40450bfbf503ad2
Tony <89355439+Sigvel@users.noreply.github.com>
2022-05-20 22:47:24 +0200

    contact page update.

--- js/contact.js
+++ js/contact.js
@@ -1,39 +1,39 @@
 // HTML form Element
 const form = document.querySelector("#contact-form");
 
+const formSuccess = document.querySelector(".form-success");
+
 // input validations
-const formName = document.querySelector("#name");
+const formName = document.querySelector("#your-name");
 const nameError = document.querySelector("#nameError");
 
-const formEmail = document.querySelector("#email");
+const formEmail = document.querySelector("#your-email");
 const emailError = document.querySelector("#emailError");
 
-const formSubject = document.querySelector("#subject");
+const formSubject = document.querySelector("#your-subject");
 const subjectError = document.querySelector("#subjectError");
 
-const formMessage = document.querySelector("#message");
+const formMessage = document.querySelector("#your-message");
 const messageError = document.querySelector("textarea");
 
-function formValidation(event) {
-  event.preventDefault();
-
+function inputValidation(event) {
   const targetId = event.target.id;
   const targetValue = event.target.value;
 
   switch (targetId) {
-    case "name": {
+    case "your-name": {
       nameError.style.background = checkInputLength(targetValue, 5) ? "darkgreen" : "darkred";
       break;
     }
-    case "email": {
+    case "your-email": {
       emailError.style.background = checkEmailValidation(targetValue) ? "darkgreen" : "darkred";
       break;
     }
-    case "subject": {
+    case "your-subject": {
       subjectError.style.background = checkInputLength(targetValue, 15) ? "darkgreen" : "darkred";
       break;
     }
-    case "message": {
+    case "your-message": {
       messageError.style.background = checkInputLength(targetValue, 25) ? "darkgreen" : "darkred";
       break;
     }
@@ -41,8 +41,8 @@ function formValidation(event) {
 }
 
 //Event listeners
-form.addEventListener("input", formValidation);
-form.addEventListener("submit", isFormValid);
+form.addEventListener("input", inputValidation);
+form.addEventListener("submit", formValidation);
 
 function checkInputLength(value, len) {
   if (value.trim().length > len) {
@@ -58,9 +58,27 @@ function checkEmailValidation(email) {
   return patternMatches;
 }
 
-function isFormValid() {
-  if (checkInputLength(formName.value, 6) && checkEmailValidation(email.value) && checkInputLength(subject.value, 16) && checkInputLength(message.value, 26)) {
-    form.innerHTML = `<div class="successMessage"><span>Form successfully sent!</span></div>`;
-    form.reset();
+function formValidation(evnt) {
+  evnt.preventDefault();
+
+  // const formElement = evnt.target;
+
+  const formContactUrl = "https://your-site.tld/wp-json/contact-form-7/v1/contact-forms/232/feedback";
+
+  if (checkInputLength(formName.value, 6) && checkEmailValidation(formEmail.value) && checkInputLength(formSubject.value, 16) && checkInputLength(formMessage.value, 26)) {
+    formSuccess.innerHTML = `<p class="successMessage">The form was submitted successfully</p>`;
   }
+
+  let formData = new FormData();
+  formData.append("your-name", formName.value);
+  formData.append("your-subject", formSubject.value);
+  formData.append("your-message", formMessage.value);
+  formData.append("your-email", formEmail.value);
+
+  fetch(formContactUrl, {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log("error", error));
 }
